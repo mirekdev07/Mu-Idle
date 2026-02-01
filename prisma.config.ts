e9d76e6367049343964a2599d33598eafdf38fed
Mirek <mirek@example.com>
2026-02-01 20:23:36 +0100

    Fix prisma.config.ts TypeScript error

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,22 +1,6 @@
 import path from 'node:path';
-import { defineConfig } from 'prisma/config';
+import type { PrismaConfig } from 'prisma';
 
-export default defineConfig({
-  earlyAccess: true,
+export default {
   schema: path.join(__dirname, 'prisma', 'schema.prisma'),
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
-  migrate: {
-    adapter: async () => {
-      const { PrismaNeon } = await import('@prisma/adapter-neon');
-      const { neonConfig, Pool } = await import('@neondatabase/serverless');
-
-      neonConfig.webSocketConstructor = (await import('ws')).default;
-
-      const connectionString = process.env.DATABASE_URL!;
-      const pool = new Pool({ connectionString });
-      return new PrismaNeon(pool);
-    },
-  },
-});
+} satisfies PrismaConfig;
